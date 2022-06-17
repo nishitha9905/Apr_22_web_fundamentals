cd5f0cd9d3413d12ac6b1ae303bc360b9910385f
nishitha <nishithakamakshikoduru@gmail.com>
2022-06-17 23:18:08 +0530

    made the javascript assignments

---
+++ Apr Assignments/practice_projects.html/Objects.js
@@ -0,0 +1,25 @@
+var student = {
+    name:{
+        firstName:"Nishitha",
+        lastName:"Koduru",
+    },
+    id:101,
+    phone:9873742235,
+    emailId:"nishi@gmail.com",
+    company:"TCS"
+}
+console.log(student);
+student.company= {
+    company1:"TCS",
+    company2:"WIPRO",
+    comapny3:"INFOSYS"
+
+}
+console.log(student);
+student.phone = [
+    phoneNumbers = {
+    mobileNumber : 769868565,
+    officeNumber :374235955,
+    }
+]
+console.log(student.phone);
